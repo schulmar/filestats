2fd7c37c9f3f8fa826bee796bbcd4739fc7f58e8
Martin Schulze <schulmar@hrz.tu-chemnitz.de>
2013-12-04 23:44:09 +0100

    Replaced files() with sinceLast instance of Files

--- io.cpp
+++ io.cpp
@@ -10,6 +10,14 @@
 #include <unistd.h>
 
 #include "files.h"
+#include "deltatimeexecutor.h"
+
+static Files sinceLast;
+static DeltaTimeExecutor executor([&]() {
+                                    printStatistics(sinceLast);
+                                    sinceLast.resetStatistics();
+                                  },
+                                  std::chrono::microseconds(1000 * 1000 * 10));
 
 extern "C" {
 
@@ -25,7 +33,8 @@ FILE *fopen(__const char *__restrict __filename,
   GET_REAL(fopen);
   FILE *fd = real_fopen(__filename, __modes);
   if (fd)
-    files().open(__filename, fileno(fd));
+    sinceLast.open(__filename, fileno(fd));
+  executor.callIfTimeOver();
   return fd;
 }
 
@@ -34,7 +43,8 @@ FILE *fopen64(__const char *__restrict __filename,
   GET_REAL(fopen64);
   FILE *fd = real_fopen64(__filename, __modes);
   if (fd)
-    files().open(__filename, fileno(fd));
+    sinceLast.open(__filename, fileno(fd));
+  executor.callIfTimeOver();
   return fd;
 }
 
@@ -53,7 +63,8 @@ size_t fread(void *__restrict __ptr, size_t __size, size_t __n,
              FILE *__restrict __stream) {
   GET_REAL(fread);
   MEASURE(real_fread(__ptr, __size, __n, __stream));
-  files().read(fileno(__stream), res * __size, diffTime);
+  sinceLast.read(fileno(__stream), res * __size, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 
@@ -61,7 +72,8 @@ size_t fwrite(__const void *__restrict __ptr, size_t __size, size_t __n,
               FILE *__restrict __s) {
   GET_REAL(fwrite);
   MEASURE(real_fwrite(__ptr, __size, __n, __s));
-  files().write(fileno(__s), res * __size, diffTime);
+  sinceLast.write(fileno(__s), res * __size, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 
@@ -69,7 +81,8 @@ size_t fread_unlocked(void *__restrict __ptr, size_t __size, size_t __n,
                       FILE *__restrict __stream) {
   GET_REAL(fread_unlocked);
   MEASURE(real_fread_unlocked(__ptr, __size, __n, __stream));
-  files().read(fileno(__stream), res * __size, diffTime);
+  sinceLast.read(fileno(__stream), res * __size, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 
@@ -77,21 +90,24 @@ size_t fwrite_unlocked(__const void *__restrict __ptr, size_t __size,
                        size_t __n, FILE *__restrict __s) {
   GET_REAL(fwrite_unlocked)
   MEASURE(real_fwrite_unlocked(__ptr, __size, __n, __s));
-  files().write(fileno(__s), res * __size, diffTime);
+  sinceLast.write(fileno(__s), res * __size, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 
 int creat(__const char *__file, __mode_t __mode) {
   GET_REAL(creat);
   int fd = real_creat(__file, __mode);
-  files().open(__file, fd);
+  sinceLast.open(__file, fd);
+  executor.callIfTimeOver();
   return fd;
 }
 
 int creat64(__const char *__file, __mode_t __mode) {
   GET_REAL(creat64);
   int fd = real_creat64(__file, __mode);
-  files().open(__file, fd);
+  sinceLast.open(__file, fd);
+  executor.callIfTimeOver();
   return fd;
 }
 
@@ -99,7 +115,8 @@ int open(__const char *__file, int __oflag, ...) {
   GET_REAL(open);
   int fd = real_open(__file, __oflag);
   if (fd != -1)
-    files().open(__file, fd);
+    sinceLast.open(__file, fd);
+  executor.callIfTimeOver();
   return fd;
 }
 
@@ -107,28 +124,32 @@ int open64(__const char *__file, int __oflag, ...) {
   GET_REAL(open64);
   int fd = real_open64(__file, __oflag);
   if (fd != -1)
-    files().open(__file, fd);
+    sinceLast.open(__file, fd);
+  executor.callIfTimeOver();
   return fd;
 }
 
 int close(int __fd) {
   GET_REAL(close);
   int ret = real_close(__fd);
-  files().close(__fd);
+  sinceLast.close(__fd);
+  executor.callIfTimeOver();
   return ret;
 }
 
 ssize_t read(int __fd, void *__buf, size_t __nbytes) {
   GET_REAL(read);
   MEASURE(real_read(__fd, __buf, __nbytes));
-  files().read(__fd, res, diffTime);
+  sinceLast.read(__fd, res, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 
 ssize_t write(int __fd, __const void *__buf, size_t __n) {
   GET_REAL(write);
   MEASURE(real_write(__fd, __buf, __n));
-  files().write(__fd, res, diffTime);
+  sinceLast.write(__fd, res, diffTime);
+  executor.callIfTimeOver();
   return res;
 }
 }
